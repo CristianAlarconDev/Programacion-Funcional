30abec3e8ea67521147dbf99c6304145f2deb8b4
CristianAlarconDev <calarcon@fi.uba.ar>
2026-04-19 13:01:00 -0300

    funciones para contar apariciones de palabras en una lista, los resultados se mapean, usa tail recursion y la v2 getOrElse para tomar 0 apariciones por defecto en caso de no encontrar la key head buscada, rehacer con funciones de orden superior

---
+++ src/main/scala/EjerciciosAvanzados.scala
@@ -0,0 +1,34 @@
+package com.calarcon.ejercicios
+
+import scala.annotation.tailrec
+
+object EjerciciosAvanzados {
+  def contar(palabras:List[String]):Map[String,Int]={
+    @tailrec
+    def auxiliar(palabras:List[String], map: Map[String, Int]):Map[String, Int]= palabras match {
+      case Nil => map
+      case head::tail if map.contains(head)  =>
+        val cantidadApariciones= map(head)
+        val nuevoMapa =map+(head->(cantidadApariciones + 1))
+        auxiliar(tail, nuevoMapa)
+      case head::tail =>
+        val mapNuevo= map + (head->1)
+        auxiliar(tail, mapNuevo)
+    }
+    val mapaVacio = Map[String, Int]()
+    auxiliar(palabras, mapaVacio)
+  }
+  def contarV2(palabras:List[String]):Map[String,Int]={
+    @tailrec
+    def auxiliar (palabras:List[String], palabrasMap:Map[String, Int]):Map[String, Int] = palabras match{
+      case Nil => palabrasMap
+      case head::tail =>
+        val cantApariciones= palabrasMap.getOrElse(head,0)
+        val nuevoMapa= palabrasMap+(head->(cantApariciones+1))
+        auxiliar(tail, nuevoMapa)
+    }
+    auxiliar(palabras, Map[String, Int]())
+    
+  }
+  
+}
