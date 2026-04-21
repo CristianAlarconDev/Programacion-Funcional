c0d9e871000e2e2821048e0c0582875535812905
Cristian <calarcon@fi.uba.ar>
2026-04-21 15:25:13 -0300

    distancia entre 2 puntos con import sqrt dentro de la funcion, aun es deterministica y no tiene side effects, usa una case class

--- src/main/scala/EjerciciosCaseClasses.scala
+++ src/main/scala/EjerciciosCaseClasses.scala
@@ -7,5 +7,13 @@ object EjerciciosCaseClasses {
   def filtrarPorPrecio(productos: List[Producto], limite: Double): List[Producto] = {
     productos.filter(producto => producto.precio < limite)
   }
+  //Ejercicio 2
+  case class PuntoCoordenada(coordenadaX: Double, coordenadaY:Double)
+  def distanciaEntre(unPunto:PuntoCoordenada, otroPunto:PuntoCoordenada):Double={
+    import scala.math.sqrt
+    val difX = otroPunto.coordenadaX - unPunto.coordenadaX
+    val difY = otroPunto.coordenadaY - unPunto.coordenadaY
+    sqrt((difX * difX) + (difY * difY))
 
+  }
 }
