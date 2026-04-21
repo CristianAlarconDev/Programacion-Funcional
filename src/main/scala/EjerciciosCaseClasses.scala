9eedbd35341b5cf041e55f937235c8206dfe0011
Cristian <calarcon@fi.uba.ar>
2026-04-21 15:17:19 -0300

    ejercicio 1 usando una case class

--- src/main/scala/EjerciciosCaseClasses.scala
+++ src/main/scala/EjerciciosCaseClasses.scala
@@ -1,6 +1,11 @@
 package com.calarcon.ejercicios
 object EjerciciosCaseClasses {
 
+  //Ejercicio 1
   case class Producto(nombre: String, precio: Double)
+
+  def filtrarPorPrecio(productos: List[Producto], limite: Double): List[Producto] = {
+    productos.filter(producto => producto.precio < limite)
+  }
 
 }
