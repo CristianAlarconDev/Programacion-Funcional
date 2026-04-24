bcc119a7c96e716d62c1a8b0cf8a090d2cba7e7c
Cristian <calarcon@fi.uba.ar>
2026-04-24 10:24:48 -0300

    intente usar tailrec, la idea es generar una lsita de un sealed trait token evaluando cada caracter con el pattern match, caso de que sea variable se usan span para dividirla y agregarla al acumulador y se procede con la iteracion; lueo escribo casos de uso

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -13,3 +13,23 @@ case class VAR(nombre: String) extends Token
 def esEspecial(c: Char): Boolean =
   c == 'λ' || c == '.' || c == ' ' || c == '(' || c == ')'
 
+def lexer(input: String): List[Token] = {
+  @tailrec
+  def auxiliar(caracteres: List[Char], acumulador: List[Token]): List[Token] = caracteres match {
+    case Nil => acumulador
+
+    case 'λ' :: tail => auxiliar(tail, acumulador :+ LAMBDA)
+    case '.' :: tail => auxiliar(tail, acumulador :+ DOT)
+    case ' ' :: tail => auxiliar(tail, acumulador :+ SPACE)
+    case '(' :: tail => auxiliar(tail, acumulador :+ LPAR)
+    case ')' :: tail => auxiliar(tail, acumulador :+ RPAR)
+
+    // caso variable
+    case head :: tail =>
+      val (nombreVar, resto) = caracteres.span(c => !esEspecial(c))
+
+      val tokenVar = VAR(nombreVar.mkString)
+      auxiliar(resto, acumulador :+ tokenVar)
+  }
+  auxiliar(input.toList, List())
+}
