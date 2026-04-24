cfbaae97c451af4b810d6d1bf779c396163e8128
Cristian <calarcon@fi.uba.ar>
2026-04-24 10:21:36 -0300

    funcion que luego voy a intentar usar para separa los casos que van dentro de una variable y los que debe tomar el pattern match

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -1,4 +1,7 @@
 package com.calarcon.ejercicios
+
+import scala.annotation.tailrec
+
 sealed trait Token
 case object LAMBDA extends Token
 case object SPACE  extends Token
@@ -6,3 +9,7 @@ case object DOT    extends Token
 case object LPAR   extends Token
 case object RPAR   extends Token
 case class VAR(nombre: String) extends Token
+
+def esEspecial(c: Char): Boolean =
+  c == 'λ' || c == '.' || c == ' ' || c == '(' || c == ')'
+
